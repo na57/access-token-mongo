512bef418b01c1b5e62da96539b68689602ad2d7
na57 <na57@qq.com>
2015-06-05 10:29:11 +0800

    修改为2.0.0版

--- index.js
+++ index.js
@@ -4,21 +4,25 @@
 
 var MongoClient = require('mongodb').MongoClient,
     ObjectID = require('mongodb').ObjectID;
-var array = require('array');
-var uuid = require('node-uuid');
 var moment = require('moment');
 
 
 /**
  * 初始化
- * @param connString
+ * @param db
  * 数据库连接字符串
  * @param collection
  * 保存access_token使用的Collection的名称，默认为'access_tokens'
  * @returns {{getToken: Function, saveToken: Function}}
  */
-module.exports = function(connString, collection){
+module.exports = function(db, collection){
     collection = collection || 'access_tokens';
+
+    var getCollection = function (callback) {
+        var col = db.collection(collection);                    // 获取集合实例
+        callback(null, col);
+    };
+
     return {
         // 获取指定的AccessToken
         /**
@@ -30,38 +34,41 @@ module.exports = function(connString, collection){
          * @param callback
          */
         getToken: function (options, callback) {
-            MongoClient.connect(connString, function (err, db) {
-                var at = db.collection(collection);
-                at.findOne({
-                    'appId': options.appId,
-                    'appSecret': options.appSecret,
-                    'expire': {'$gt': new Date()}
-                }, function (err, at) {
-                    db.close();
-                    if(at == null){
-                        callback('error');
-                    } else {
-                        callback(err, at.token);
-                    }
-                });
+            getCollection(function (err, col) {
+                if(err) callback(err);
+                else {
+                    col.findOne({
+                        'appId': options.appId,
+                        'appSecret': options.appSecret,
+                        'expire': {'$gt': new Date()}
+                    }, function (err, at) {
+                        if(err || at == null){
+                            callback('error');
+                        } else {
+                            callback(err, at.token);
+                        }
+                    });
+                }
             });
         },
         saveToken: function (options, token, callback) {
             options.expire = options.expire || 7000;
-            MongoClient.connect(connString, function (err, db) {
-                var at = db.collection(collection);
-                at.update({
-                    'appId': options.appId,
-                    'appSecret': options.appSecret
-                }, {
-                    'appId': options.appId,
-                    'appSecret': options.appSecret,
-                    'expire': moment().add(options.expire, 's').toDate(),
-                    'token': token
-                }, {upsert: true}, function (err, doc) {
-                    db.close();
-                    callback(err, doc);
-                });
+
+            getCollection(function (err, col) {
+                if(err) callback(err);
+                else {
+                    col.update({
+                        'appId': options.appId,
+                        'appSecret': options.appSecret
+                    }, {
+                        'appId': options.appId,
+                        'appSecret': options.appSecret,
+                        'expire': moment().add(options.expire, 's').toDate(),
+                        'token': token
+                    }, {upsert: true}, function (err, doc) {
+                        callback(err, doc);
+                    });
+                }
             });
         }
     };
